e2501f8c69dbfb5fdef5a07a7668eaec56b14df3
Ömer Can Demirel <omrcndmrl041@hotmail.com>
2024-09-24 09:28:45 +0300

    End of section 12

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,37 +1,77 @@
-import { Component, inject, input, output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule, CommonModule, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   private accountService = inject(AccountService);
-  private toastr = inject(ToastrService);
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  maxDate = new Date();
+  validationErrors: string[] | undefined;
 
   // @Input() usersFromHomeComponent: any *before Angular 17.3
   // usersFromHomeComponent = input.required<any>(); this is an signal extension
 
   // @Output() cancelRegister = new EventEmitter(); *before Angular 17.3
   cancelRegister = output<boolean>(); // this is an signal extension
-  model: any ={};
+  registerForm: FormGroup = new FormGroup({});
+
+  initializeForm(){
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ["", Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ["", [Validators.required, this.matchValues("password")]]
+    });
+
+    // Inputlar arasında iki taraflı kontrolü sağlamak için aşağıdaki işlem yapılır.
+    this.registerForm.controls["password"].valueChanges.subscribe({
+      next: () => this.registerForm.controls["confirmPassword"].updateValueAndValidity()
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
+  }
 
   register(){
-    this.accountService.register(this.model).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => this.toastr.error(error.error)
-    })
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+    const values = this.registerForm.value;
+    values.dateOfBirth = dob;
+    this.accountService.register(values).subscribe({
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.validationErrors = error
+    });
   }
 
   cancel(){
     this.cancelRegister.emit(false)
+  }
+
+  private getDateOnly(dob: string | undefined){
+    if (!dob) return;
+    return new Date(dob).toISOString().slice(0, 10);
+  }
+
+  ngOnInit(): void {
+    this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear()- 18);
   }
 }
