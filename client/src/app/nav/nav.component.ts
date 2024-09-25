cb0de8b3789e0ef9007d26690b25083c8edbca54
Ömer Can Demirel <omrcndmrl041@hotmail.com>
2024-09-25 14:04:41 +0300

    End of section 13

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,7 +4,6 @@ import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
@@ -13,8 +12,7 @@ import { TitleCasePipe } from '@angular/common';
     FormsModule,
     BsDropdownModule,
     RouterLink,
-    RouterLinkActive,
-    TitleCasePipe
+    RouterLinkActive
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
